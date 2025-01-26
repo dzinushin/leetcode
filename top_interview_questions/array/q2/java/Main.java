a7b9b1e1c0d594d5039a19d44784d9ac7ad4598d
Dmitry Zinushin <zinushin@gmail.com>
2025-01-26 15:54:36 +0300

    new task

---
+++ top_interview_questions/array/q2/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - prices[i - 1];
+            if (profit > 0) {
+                maxProfit += profit;
+            }
+        }
+        return maxProfit;
+    }
+}
